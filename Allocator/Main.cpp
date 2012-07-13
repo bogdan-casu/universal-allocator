431c6315e804f7cd86a15270f5f348bd59c0bca7
bogdan-casu <bogdan.casu-pop@deskperience.com>
2012-07-13 17:21:43 +0300

    working

--- Allocator/Main.cpp
+++ Allocator/Main.cpp
@@ -2,11 +2,22 @@
 //
 
 #include "stdafx.h"
+#include "Allocator.h"
+#include <stdio.h>
+
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	std::cin.get();
+
+	for (int i = 0; i < 100; i++){
+		Allocator<int> vector(10000000);
+		vector[9000000] = 100; 
+		//std::cout << vector[2];
+	}
 
 
+	system("PAUSE");
 	return 0;
 }
