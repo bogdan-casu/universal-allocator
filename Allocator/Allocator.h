4ad590f1b5ce98e5fa270e682e339370ac112e9f
bogdan-casu <bogdan.casu-pop@deskperience.com>
2012-07-13 17:21:05 +0300

    working

--- Allocator/Allocator.h
+++ Allocator/Allocator.h
@@ -1,19 +1,50 @@
+#include <iostream>
+#include <memory>
+#include <exception>
 
+#define i_STD_SIZE 32
 
-template < class T >
+template <class T>
 class Allocator
 {
-	#define i_STD_SIZE 32
 
 protected:
 	size_t m_iCapacity;
-	size_t m_iAllocated;
 	size_t m_iBASE_SIZE;
-	shared_ptr < T > m_pData;
+	std::tr1::shared_ptr<T> m_pData;
 
-	Allocator() : m_iBASE_SIZE(i_STD_SIZE), data(new T[i_STD_SIZE])
+	Allocator() : m_iBASE_SIZE(i_STD_SIZE), m_pData(new T[i_STD_SIZE])
 	{
-	
 	}
 
-}
+public:
+
+	Allocator(int baseSize) : m_iBASE_SIZE(baseSize), m_pData(new T[baseSize])
+	{
+	}
+
+	T& operator[] (size_t index)
+	{
+		if (index < m_iCapacity)
+		{
+			return  *m_pData.get();
+		}
+		else
+		{
+			throw -1;
+		}
+	}
+
+	const T& operator[](size_t index) const
+	{
+		if (index < m_iCapacity)
+		{
+			return m_pData[index];
+		}
+		else
+		{
+			throw -1;
+		}
+	}
+
+};
